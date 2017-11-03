46a17dfad6712f4148ace1d2bbd1b3ba78dccc82
Amar Arora <amararora07@gmail.com>
2017-11-03 13:27:45 +0530

    Create new file

---
+++ fractionDivision.py
@@ -0,0 +1,10 @@
+def fractionDivision(a, b):
+    c=[a[0]*b[1],a[1]*b[0]]
+    gcd2=gcd(c[0],c[1])
+    c[0]/=gcd2
+    c[1]/=gcd2
+    return c
+def gcd(a,b):
+    if a==0:
+        return b
+    return gcd(b%a,a)
