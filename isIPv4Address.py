cb9d2bca0ab3795d5304743aff3718793bd58197
Amar Arora <amararora07@gmail.com>
2017-11-14 19:30:57 +0530

    Update isIPv4Address.py

--- isIPv4Address.py
+++ isIPv4Address.py
@@ -1,11 +1,11 @@
 def isIPv4Address(inp):
     l=inp.split(".")
     print l
-    if len(s)==4:
+    if len(l)==4:
         x=0
         for i in l:
             if i.isdigit() and 0<=int(i)<=255:
                 x+=1
-        if x==len(s):
+        if x==len(l):
             return True
     return False
