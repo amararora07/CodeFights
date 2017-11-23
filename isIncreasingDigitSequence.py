e80b9dff18b5f107021b357c89517aa6c0d932b0
Amar Arora <amararora07@gmail.com>
2017-11-23 22:09:54 +0530

    Update isIncreasingDigitSequence.py

--- isIncreasingDigitSequence.py
+++ isIncreasingDigitSequence.py
@@ -1,13 +1,6 @@
 def isIncreasingDigitsSequence(n):
-    if n==0:
-        return True
-    l=[]
-    while n > 0:
-        l.append(n%10)
-        n/=10
-    m=l[::-1]
-    for i in range(len(m)-1):
-        if m[i] >= m[i+1]:
+    l=list(str(n))
+    for i in range(len(l)-1):
+        if ord(l[i])>=ord(l[i+1]):
             return False
-    else:
-        return True
+    return True
