d944fb718b192343600f8422af3e7f5f53414126
Amar Arora <amararora07@gmail.com>
2017-12-01 20:16:40 +0530

    Create metroCard.py

---
+++ metroCard.py
@@ -0,0 +1,8 @@
+def metroCard(l):
+    if l==28 or l==30 or l==31:
+        if l==28:
+            return [31]
+        elif l==30:
+            return [31]
+        else:
+            return [28,30,31]
